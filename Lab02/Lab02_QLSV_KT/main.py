25ccfebab2c0e9cb9977d56407ca6f0b6f0a88a4
dominhdang148 <myclone235@gmail.com>
2022-09-01 11:12:23 +0700

    Cập nhập bài 5

--- Lab02/Lab02_QLSV_KT/main.py
+++ Lab02/Lab02_QLSV_KT/main.py
@@ -9,5 +9,13 @@ system('cls')
 danhsach = DanhSachSinhVien()
 danhsach.ReadFile("dssv.txt")
 print(danhsach)
+
+
+result=danhsach.TimSinhVienCoDiemRenLuyenTren(80)
+print("Danh sách sinh viên có điểm rèn luyện trên 80 là:")
+print(result)
+
+
 result=danhsach.TimSinhVienTrinhDoSinhTruoc("Cao đẳng", '15/8/1999')
+print("Danh sách sinh viên phi chính quy có trình độ Cao đằng sinh trước 15/8/1999 là: ")
 print(result)
